4dc396fa7a5e9838072fcc9b64142a563e432f18
Luis Vilches <lvilches21@gmail.com>
2016-11-19 02:43:17 -0300

    m

--- angular/controllers/index.js
+++ angular/controllers/index.js
@@ -14,4 +14,36 @@ angular.module("dowhile",[])
 		console.log(err);
 
 	});
+})
+
+.controller('about', function($scope, $http){
+	$http({
+		method: "GET",
+		url: "http://localhost:4000/api/about"
+	}).then(function(response){
+		console.log(response.data.about);
+		$scope.about = response.data.about;
+
+
+	}, function(err){
+
+		console.log(err);
+
+	});
+})
+
+.controller('header', function($scope, $http){
+	$http({
+		method: "GET",
+		url: "http://localhost:4000/api/header"
+	}).then(function(response){
+		console.log(response.data.header);
+		$scope.header = response.data.header;
+
+
+	}, function(err){
+
+		console.log(err);
+
+	});
 });
